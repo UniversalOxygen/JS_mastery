325caf9f6fa63f05e1a21a3f55c6cacbbe709fe0
unknown <olagoketunde600@gmail.com>
2023-01-05 15:32:53 -0600

    what if?

---
+++ dupl.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const dupl = () => {
+  return (
+
+    const btn = document.querySelector('button')
+btn.addEventListener ('click', showMe)
+
+function showMe() {
+    alert('My Name is OXYGEN')
+}
+    <div>dupl</div>
+  )
+}
+
+export default dupl
