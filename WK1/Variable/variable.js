12162501419e0176116298ce7a92c5663dd3a405
unknown <olagoketunde600@gmail.com>
2023-01-10 15:55:50 -0600

    if statement

--- WK1/Variable/variable.js
+++ WK1/Variable/variable.js
@@ -1,8 +1,12 @@
 const userName = "Universal Oxygen"
 
 function UserDetails() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i)
+        console.log("This account belongs to " + userName)
+    }
     alert("This account belongs to " + userName)
-    console.log("This account belongs to " + userName)
+    
 }
 
 UserDetails()
