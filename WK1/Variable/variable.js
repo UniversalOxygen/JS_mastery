b3a07bb82792258f3647af04ab5fcfc5c5c95a4f
unknown <olagoketunde600@gmail.com>
2023-01-11 16:22:22 -0600

    if statement

--- WK1/Variable/variable.js
+++ WK1/Variable/variable.js
@@ -16,3 +16,17 @@ const object = { name: "John", age: 20 };
 for (const key in object) {
     console.log(key)
 }
+
+
+//if statement
+let day = "Monday"
+if (day == "Monday") alert("Today is Monday")
+
+let size = 25
+
+if (size == 20) {
+    alert("Your size is 20")
+    alert("Thank you!")
+}
+
+
