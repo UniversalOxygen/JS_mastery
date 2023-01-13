33c5f6dd4bd871cfbfce786966fb01a0000dfd8f
unknown <olagoketunde600@gmail.com>
2023-01-13 17:57:18 -0500

    quick

--- WK1/Variable/variable.js
+++ WK1/Variable/variable.js
@@ -28,3 +28,12 @@ if (size == 20) {
     alert("Your size is 20")
     alert("Thank you!")
 }
+
+
+//if-else statement
+if (size == 20) {
+    alert("Size is available")
+    alert("Thank you!")
+} else {
+    alert("The size is not available")
+}
